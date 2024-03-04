53e9b9d7c4b9e5d3d15dfe829177ca5c12d21127
ngocpd2001 <phanngoc2213@gmail.com>
2024-03-04 23:32:48 +0700

    fix footer & user drop dowm

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.jsx";
 import { ToastContainer } from "react-toastify";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
