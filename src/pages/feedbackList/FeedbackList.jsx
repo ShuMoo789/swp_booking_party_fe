4a75543eb2e885589fbda1ddec38bd1008921890
duongtoan106 <phamduongtoan106@gmail.com>
2024-03-24 17:56:31 +0700

    fix dashboard

--- src/pages/feedbackList/FeedbackList.jsx
+++ src/pages/feedbackList/FeedbackList.jsx
@@ -12,7 +12,7 @@ const FeedbackList = () => {
 
   const fetchFeedbacks = async () => {
     try {
-      const response = await api.get("/feedbacks");
+      const response = await api.get("/feedback");
       setFeedbacks(response.data);
     } catch (error) {
       console.error("Error fetching feedbacks:", error);
